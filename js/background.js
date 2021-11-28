eb5255cefda3296e02bdc50745a6323f61561fc6
seoltaehyeok <ajaj312216@gmail.com>
2021-11-29 05:36:15 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@ const images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"]
 const chsenImage = images[Math.floor(Math.random() * images.length)];
 
 // const img = document.body.appendChild(chsenImage); // html 요소 추가
-document.body.style.backgroundImage = `url("../img/${chsenImage}")`;
+document.body.style.backgroundImage = `url("./img/${chsenImage}")`;
 // <style>
 //     body {
 //       background-image: url('img/3.jpg');
